bb18afa3a3d073c251c2ab34392e44dccab8e8dc
ajjeon <a0071251@nus.edu.sg>
2013-02-15 16:33:45 +0800

    practice02 bash code for AMPD

---
+++ extract.sh
@@ -0,0 +1,22 @@
+#1/bin/bash
+
+# TODO : figure out how to fix digits e.g. 1(X) 00001 (O)
+
+
+
+ for idx in $(seq 5)
+ do
+    curl aps.unmc.edu/AP/database/query_output.php?ID=$idx | head -53 | tail -1 | cut -d ">" -f 4 | cut -d "<" -f1 >> test.practice
+ done
+
+#  WARNING : n value in R is set to be 5 for now.
+
+
+
+# rscript extract.R
+
+
+
+# appending file in bash, open the file and write/ append the file without overwriting
+# writing into the variable and write all the variables together into a file
+
